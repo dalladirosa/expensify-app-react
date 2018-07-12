d03aee1054cc97f26dddfc89e1ae8392ef0b78c2
Dalla Dirosa <dalladirosa@gmail.com>
2018-07-12 15:01:48 +0700

    Setup production build and server(3rd)

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = (env) => {
   return {
     entry: './src/app.js',
     output: {
-      path: path.join(__dirname, 'public'),
+      path: path.join(__dirname, '/public'),
       filename: 'bundle.js'
     },
     module: {
